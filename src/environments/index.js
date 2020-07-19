a684a63a38c0b4ec8e0e32cd329622810913f027
cbcarlos07 <cbcarlos08@gmail.com>
2020-07-18 22:39:40 -0400

    -

--- src/environments/index.js
+++ src/environments/index.js
@@ -1,6 +1,6 @@
 const env = {
-    host: 'http://localhost:3001',
-    //host: 'http://yunieapp.com.br:21345',
+   // host: 'http://localhost:3001',
+    host: 'http://yunieapp.com.br:21345',
     endpoint: '/yunie/v1'
 }
 export default env
